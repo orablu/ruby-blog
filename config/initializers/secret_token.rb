797aa9a161452fdbafd871e690446f5b08f9a3d2
Wes <ora@outlook.com>
2013-12-23 00:26:29 -0500

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Blog::Application.config.secret_key_base = '76f58941e6aa4c436c121244b5113ce17b360dbf31c38ea5bfbf6a745bcda33894a3b553e4b48f1fe17babf646f371476d17646ba05f4fae0e47392f55b2d87b'
